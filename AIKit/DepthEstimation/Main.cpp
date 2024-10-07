1b3d4312b716b854bafc679ffdffca283fc0beca
horinoh <development@horino.sakura.ne.jp>
2024-10-07 23:59:12 +0900

    コード更新

--- AIKit/DepthEstimation/Main.cpp
+++ AIKit/DepthEstimation/Main.cpp
@@ -18,126 +18,142 @@
 
 //#define OUTPUT_VIDEO
 
-void Inference(std::vector<hailort::InputVStream> &In, std::vector<hailort::OutputVStream> &Out, const char* InVideo) {
-	auto IsRunning = true;
-
-	//!< 同期用
-	auto InFrame = 0;
-	auto OutFrame = 0;
-
-	constexpr auto Fps = 30;
-
-	//!< AI への入力
-	//std::mutex InMutex;
-	const auto& InShape = In.front().get_info().shape;
-	cv::Mat ColorMap;
-	auto InThread = std::thread([&]() {
-		cv::Mat InAI;
-
-		//!< カメラ : VideoCapture() へ Gstreamer 引数を渡す形で作成すれば良い (OpenCV では "format=BGR" を追加指定する必要があるので注意)
+class Hailo 
+{
+public:
+	static std::string GetLibCameGSTStr(const int Width, const int Height, const int FPS) {
 #if false
-		const auto LibCam = std::format("libcamerasrc ! video/x-raw, width={}, height={}, framerate={}/1, format=BGR, ! appsink", InShape.width, InShape.height, Fps);
+		return std::format("libcamerasrc ! video/x-raw, width={}, height={}, framerate={}/1, format=BGR, ! appsink", Width, Height, FPS);
 #else
 		std::stringstream SS;
-		SS << "libcamerasrc ! video/x-raw, width=" << InShape.width  << ", height=" << InShape.height << ", framerate=" << Fps << "/1, format=BGR, ! appsink";	
-		const auto LibCam = SS.str();
+		SS << "libcamerasrc ! video/x-raw, width=" << Width  << ", height=" << Height << ", framerate=" << FPS << "/1, format=BGR, ! appsink";	
+		return SS.str();
 #endif
+	}
 
-		//!< キャプチャ : 入力ファイルの指定が無い場合はカメラ
-		cv::VideoCapture Capture(nullptr != InVideo ? InVideo : std::data(LibCam));
+	virtual void Start(std::string_view HefFile, const char* InVideo) {
+		//!< デバイス
+		const auto Device = hailort::Device::create_pcie(hailort::Device::scan_pcie().value()[0]);
 
-		constexpr auto Bpp = 1;
+		//!< ネットワーク
+		auto Hef = hailort::Hef::create(std::data(HefFile));
+		const auto ConfigureParams = Hef->create_configure_params(HAILO_STREAM_INTERFACE_PCIE).value();
+		auto ConfiguredNetworkGroup = Device.value()->configure(Hef.value(), ConfigureParams)->at(0);
+		
+		//!< 入出力パラメータ
+		const auto InputParams = ConfiguredNetworkGroup->make_input_vstream_params(true, HAILO_FORMAT_TYPE_UINT8, HAILO_DEFAULT_VSTREAM_TIMEOUT_MS, HAILO_DEFAULT_VSTREAM_QUEUE_SIZE).value();
+		const auto OutputParams = ConfiguredNetworkGroup->make_output_vstream_params(false, HAILO_FORMAT_TYPE_FLOAT32, HAILO_DEFAULT_VSTREAM_TIMEOUT_MS, HAILO_DEFAULT_VSTREAM_QUEUE_SIZE).value();
 
-		//!< スレッド自身に終了判断させる
-		while (IsRunning) {
-			//!< 入力と出力がズレていかないように同期する
-			if(InFrame > OutFrame) {
-				continue;
-			}
-			++InFrame;
+		//!< 入出力 (入力へ書き込むと AI に処理されて出力に返る)
+		auto InputVstreams = hailort::VStreamsBuilder::create_input_vstreams(*ConfiguredNetworkGroup, InputParams);
+		auto OutputVstreams = hailort::VStreamsBuilder::create_output_vstreams(*ConfiguredNetworkGroup, OutputParams);
 
-			//!< フレームを取得
-			//InMutex.lock();
-			{
+		//!< AI ネットワークのアクティベート
+		const auto ActivatedNetworkGroup = ConfiguredNetworkGroup->activate();
+
+		Inference(InputVstreams.value(), OutputVstreams.value(), InVideo);
+	}
+
+	virtual void Inference([[maybe_unused]] std::vector<hailort::InputVStream> &In, [[maybe_unused]] std::vector<hailort::OutputVStream> &Out, [[maybe_unused]] const char* InVideo) {		
+	}
+
+protected:
+	std::vector<std::thread> Threads;
+};
+class DepthEstimation : public Hailo
+{
+private:
+	using Super = Hailo;
+
+public:
+	virtual void Inference(std::vector<hailort::InputVStream> &In, std::vector<hailort::OutputVStream> &Out, const char* InVideo) override {
+		//!< AI 入力スレッド
+		Threads.emplace_back([&]() {
+			const auto& InShape = In.front().get_info().shape;
+			//!< カメラ : VideoCapture() へ Gstreamer 引数を渡す形で作成すれば良い (OpenCV では "format=BGR" を追加指定する必要があるので注意)
+			const auto LibCam = Super::GetLibCameGSTStr(InShape.width, InShape.height, Fps);
+			//!< キャプチャ : 入力ファイルの指定が無い場合はカメラ
+			cv::VideoCapture Capture(nullptr != InVideo ? InVideo : std::data(LibCam));
+
+			cv::Mat InAI;
+			//!< スレッド自身に終了判断させる
+			while (IsRunning) {
+				//!< 入力と出力がズレていかないように同期する
+				if(InFrameCount > OutFrameCount) { continue; }
+				++InFrameCount;
+
+				//!< フレームを取得
 				Capture >> ColorMap;
+
+				//!< リサイズ
+				if(static_cast<uint32_t>(ColorMap.cols) != InShape.width || static_cast<uint32_t>(ColorMap.rows) != InShape.height) {
+            		cv::resize(ColorMap, InAI, cv::Size(InShape.width, InShape.height), cv::INTER_AREA);
+				} else {
+					InAI = ColorMap.clone();
+				}
+
+				//!< AI への入力 (書き込み)
+				constexpr auto Bpp = 1;
+				In[0].write(hailort::MemoryView(InAI.data, InShape.width * InShape.height * InShape.features * Bpp));
 			}
-			//InMutex.unlock();
+		});
 
-			//!< リサイズ
-			if(static_cast<uint32_t>(ColorMap.cols) != InShape.width || static_cast<uint32_t>(ColorMap.rows) != InShape.height) {
-            	cv::resize(ColorMap, InAI, cv::Size(InShape.width, InShape.height), cv::INTER_AREA);
-			} else {
-				InAI = ColorMap.clone();
-			}
+		//!< AI 出力スレッド
+		Threads.emplace_back([&](){
+			const auto& OutShape = Out.front().get_info().shape;
+	
+			std::vector<uint8_t> OutAI(Out[0].get_frame_size());
+			//!< スレッド自身に終了判断させる
+			while (IsRunning) {
+				++OutFrameCount;
 
-			//!< AI への入力 (書き込み)
-			In[0].write(hailort::MemoryView(InAI.data, InShape.width * InShape.height * InShape.features * Bpp));
-		}		
-	});
+				//!< 出力を取得
+				Out[0].read(hailort::MemoryView(std::data(OutAI), std::size(OutAI)));
+				//!< OpenCV 形式へ
+				const auto CVOutAI = cv::Mat(OutShape.height, OutShape.width, CV_32F, std::data(OutAI));
 
-	//!< AI からの出力
-	std::mutex OutMutex;
-	const auto& OutShape = Out.front().get_info().shape;
-	auto DepthMap = cv::Mat(OutShape.height, OutShape.width, CV_32F, cv::Scalar(0));
-	auto OutThread = std::thread([&]() {
-		std::vector<uint8_t> OutAI(Out[0].get_frame_size());
-
-		//!< スレッド自身に終了判断させる
-		while (IsRunning) {
-			++OutFrame;
-
-			//!< 出力を取得
-			Out[0].read(hailort::MemoryView(std::data(OutAI), std::size(OutAI)));
-			//!< OpenCV 形式へ
-			const auto CVOutAI = cv::Mat(OutShape.height, OutShape.width, CV_32F, std::data(OutAI));
-				
-			OutMutex.lock();
-			//!< 深度マップの調整
-			{
-				//!< -CVOutAI を指数として自然対数の底 e のべき乗が DepthMap に返る
-				cv::exp(-CVOutAI, DepthMap);
-    			DepthMap = 1 / (1 + DepthMap);
-    			DepthMap = 1 / (DepthMap * 10 + 0.009);
+				OutMutex.lock(); {
+					//!< 深度マップの調整
+					DepthMap = cv::Mat(OutShape.height, OutShape.width, CV_32F, cv::Scalar(0));
+					//!< -CVOutAI を指数として自然対数の底 e のべき乗が DepthMap に返る
+					cv::exp(-CVOutAI, DepthMap);
+    				DepthMap = 1 / (1 + DepthMap);
+    				DepthMap = 1 / (DepthMap * 10 + 0.009);
     
-				double Mn, Mx;
-    			cv::minMaxIdx(DepthMap, &Mn, &Mx);
-				//!< 手前が黒、奥が白
-    			//DepthMap.convertTo(DepthMap, CV_8U, 255 / (Mx - Mn), -Mn);
-				//!< 手前が白、奥が黒 (逆)
-    			DepthMap.convertTo(DepthMap, CV_8U, -255 / (Mx - Mn), -Mn + 255);
+					double Mn, Mx;
+    				cv::minMaxIdx(DepthMap, &Mn, &Mx);
+					//!< 手前が黒、奥が白
+    				//DepthMap.convertTo(DepthMap, CV_8U, 255 / (Mx - Mn), -Mn);
+					//!< 手前が白、奥が黒 (逆)
+    				DepthMap.convertTo(DepthMap, CV_8U, -255 / (Mx - Mn), -Mn + 255);
+				} OutMutex.unlock();
 			}
-			OutMutex.unlock();
-		}
-	});
+		});
 
-	constexpr auto ESC = 27;
-	//!< 表示用
-	cv::Mat L, R, LR;
-	const auto LSize = cv::Size(320, 240); //!< 左のサイズ (右も同じ)
-
+#if true
+		//!< メインループ
+		constexpr auto ESC = 27;
+		//!< 表示用
+		cv::Mat L, R, LR;
+		const auto LSize = cv::Size(320, 240); //!< 左のサイズ (右も同じ)
 #ifdef OUTPUT_VIDEO
-	//!< ビデオ書き出し
-	const auto Fourcc = cv::VideoWriter::fourcc('m', 'p', '4', 'v');
-	cv::VideoWriter WriterL("RGB.mp4", Fourcc, Fps, LSize);
-	cv::VideoWriter WriterR("D.mp4", Fourcc, Fps, LSize, true);
+		//!< ビデオ書き出し
+		const auto Fourcc = cv::VideoWriter::fourcc('m', 'p', '4', 'v');
+		cv::VideoWriter WriterL("RGB.mp4", Fourcc, Fps, LSize);
+		cv::VideoWriter WriterR("D.mp4", Fourcc, Fps, LSize, true);
 #endif
+		while(Running()) {
+			const auto& CM = GetColorMap();
+			const auto& DM = GetDepthMap();
+			if(CM.empty() || DM.empty()) { continue; }
 
-	//!< メインループ
-	while(IsRunning) {
-		if(!ColorMap.empty() && !DepthMap.empty()) {
 			//!< 左 : カラーマップ
-			//InMutex.lock();
-			{
-				cv::resize(ColorMap, L, LSize, cv::INTER_AREA);
-			}
-			//InMutex.unlock();
+			cv::resize(CM, L, LSize, cv::INTER_AREA);
 
 			//!< 右 : 深度マップ
-			OutMutex.lock();
-			{
-				cv::resize(DepthMap, R, LSize, cv::INTER_AREA);
-			}
-			OutMutex.unlock();
+			OutMutex.lock(); {
+				cv::resize(DM, R, LSize, cv::INTER_AREA);
+			} OutMutex.unlock();
 
 			//!< 連結する為に左右のタイプを 8UC3 に合わせる
 			cv::cvtColor(R, R, cv::COLOR_GRAY2BGR);
@@ -152,61 +168,38 @@ void Inference(std::vector<hailort::InputVStream> &In, std::vector<hailort::Outp
 			//!< 書き出し
 			WriterL << L;
 			WriterR << R;
+#endif	
+			//!< ループを抜けます
+			if(ESC == cv::pollKey()) {
+				Exit();
+			}
+		}
+
+		Join();
 #endif
-		}
-		
-		//!< ループを抜けます
-		if(ESC == cv::pollKey()) {
-			IsRunning = false;
-		}
 	}
 
-	InThread.join();
-	OutThread.join();
-}
-
-#if false
-class Hailo 
-{
-public:
-	Hailo(std::string_view HefFile) {
-		//!< デバイス
-		const auto Device = hailort::Device::create_pcie(hailort::Device::scan_pcie().value()[0]);
-
-		//!< ネットワーク
-		auto Hef = hailort::Hef::create(std::data(HefFile)); 
-		const auto ConfiguareParam = Hef->create_configure_params(HAILO_STREAM_INTERFACE_PCIE).value();
-		ConfiguredNetworkGroup = Device.value()->configure(Hef.value(), ConfiguareParam)->at(0);
-
-		const auto InputParam = ConfiguredNetworkGroup->make_input_vstream_params(true, HAILO_FORMAT_TYPE_UINT8, HAILO_DEFAULT_VSTREAM_TIMEOUT_MS, HAILO_DEFAULT_VSTREAM_QUEUE_SIZE).value();
-		auto InputVstreams = hailort::VStreamsBuilder::create_input_vstreams(*ConfiguredNetworkGroup, InputParam);
-
-		const auto OutputParam = ConfiguredNetworkGroup->make_output_vstream_params(false, HAILO_FORMAT_TYPE_FLOAT32, HAILO_DEFAULT_VSTREAM_TIMEOUT_MS, HAILO_DEFAULT_VSTREAM_QUEUE_SIZE).value();
-		auto OutputVstreams = hailort::VStreamsBuilder::create_output_vstreams(*ConfiguredNetworkGroup, OutputParam);
-
-		InputVstreams1 = &InputVstreams.value();
-		OutputVstreams1 = &OutputVstreams.value();	
+	void Join() {
+		for(auto& i : Threads) { i.join(); }
 	}
 
-	virtual void Start() {
-		//!< AI ネットワークのアクティベート
-		const auto ActivatedNetworkGroup = ConfiguredNetworkGroup->activate();
+	bool Running() const { return IsRunning; }
+	void Exit(){ IsRunning = false; }
 
-		Inference(*InputVstreams1, *OutputVstreams1, nullptr);
-	}
-
-	virtual void Inference([[maybe_unused]] std::vector<hailort::InputVStream> &In, [[maybe_unused]] std::vector<hailort::OutputVStream> &Out, [[maybe_unused]] const char* InVideo) {		
-	}
+	const cv::Mat& GetColorMap() const { return ColorMap; }
+	const cv::Mat& GetDepthMap() const { return DepthMap; }
 
 protected:
-	std::shared_ptr<hailort::ConfiguredNetworkGroup> ConfiguredNetworkGroup;
+public:
+	static const int Fps = 30;
+	bool IsRunning = true;
+	int InFrameCount = 0;
+	int OutFrameCount = 0;
 
-	std::vector<hailort::InputVStream>* InputVstreams1;
-	std::vector<hailort::OutputVStream>* OutputVstreams1;
-
-	std::vector<std::thread> Threads;
+	std::mutex OutMutex;
+	cv::Mat ColorMap;
+	cv::Mat DepthMap;
 };
-#endif
 
 int main(int argc, char* argv[]) {
 	//!< 引数
@@ -214,29 +207,16 @@ int main(int argc, char* argv[]) {
 	for(int Index = 0; auto i : Args){
 		std::cout << "Args[" << Index++ << "] " << i << std::endl;
 	}
-
-	//!< デバイス
-	const auto Device = hailort::Device::create_pcie(hailort::Device::scan_pcie().value()[0]);
-
-	//!< ネットワーク
-	std::string_view HefFile("scdepthv3.hef");
-	auto Hef = hailort::Hef::create(std::data(HefFile)); 
-	const auto ConfigureParam = Hef->create_configure_params(HAILO_STREAM_INTERFACE_PCIE).value();
-	const auto ConfiguredNetworkGroup = Device.value()->configure(Hef.value(), ConfigureParam)->at(0);
-
-	//!< AI 入出力 (入力へ書き込むと AI に処理されて出力に返る)
-	const auto InputParam = ConfiguredNetworkGroup->make_input_vstream_params(true, HAILO_FORMAT_TYPE_UINT8, HAILO_DEFAULT_VSTREAM_TIMEOUT_MS, HAILO_DEFAULT_VSTREAM_QUEUE_SIZE).value();
-	auto InputVstreams = hailort::VStreamsBuilder::create_input_vstreams(*ConfiguredNetworkGroup, InputParam);
-	const auto OutputParam = ConfiguredNetworkGroup->make_output_vstream_params(false, HAILO_FORMAT_TYPE_FLOAT32, HAILO_DEFAULT_VSTREAM_TIMEOUT_MS, HAILO_DEFAULT_VSTREAM_QUEUE_SIZE).value();
-	auto OutputVstreams = hailort::VStreamsBuilder::create_output_vstreams(*ConfiguredNetworkGroup, OutputParam);
-
-	//!< AI ネットワークのアクティベート
-	const auto ActivatedNetworkGroup = ConfiguredNetworkGroup->activate();
-
 	//!< 入力ファイルパス(引数から取得)、引数指定が無い場合はカメラ画像を使用
 	const auto InVideo = std::size(Args) > 1 ? Args[1] : nullptr;
-	//!< 推論の起動
-	Inference(InputVstreams.value(), OutputVstreams.value(), InVideo);
+
+	//!< 深度推定クラス
+	DepthEstimation DE;
+	
+	//!< 推定開始
+	DE.Start("scdepthv3.hef", InVideo);
+
+	//DE.Join();
 
 	exit(EXIT_SUCCESS);
 }
